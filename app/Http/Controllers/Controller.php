a17e19cc251ac0609ccbb6cdcbb2bed044e01eae
zeina1i <hosseinzeinalii@gmail.com>
2021-05-29 21:15:38 +0430

    Add getValidationErrorsString helper to Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Cart;
+use App\Model\CartItem;
 use App\Service\BasketService;
+use App\Service\PricingService;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,4 +14,18 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private $pricingService;
+
+    public function __construct(
+        PricingService $pricingService
+    )
+    {
+        $this->pricingService = $pricingService;
+    }
+
+    public function getValidationErrorsString(array $errors) : string
+    {
+        return implode(', ', $errors);
+    }
 }
